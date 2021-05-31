72792a3dac8c5f9e533f44de2ba649c4ac5b7c13
Declan <62638427+DevDeclann@users.noreply.github.com>
2021-06-01 08:49:01 +1200

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -4,7 +4,6 @@ import socket
 #INET raw socket packet
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-
 #receiving packets from 65565, udp/tcp
 while True:
   print(s.recvfrom(65565)) 
